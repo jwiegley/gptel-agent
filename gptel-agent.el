4974961b7000e0b2c6c0168ae2405f12abc5a122
Karthik Chikmagalur <karthikchikmagalur@gmail.com>
2025-11-16 20:26:01 -0800

    gptel-agent: Improve package commentary
    * gptel-agent.el: Change wording in package commentary to be
    closer to the README.

--- gptel-agent.el
+++ gptel-agent.el
@@ -26,10 +26,16 @@
 
 ;;; Commentary:
 
-;; Agentic use mode for gptel: This is a collection of tools and prompts for
-;; gptel to enable easy agentic LLM use.
+;; This is a collection of tools and prompts to use gptel "agentically" with any
+;; LLM, to autonomously perform tasks.
 ;;
-;; How to use:
+;; It has access to
+;; - the web (via basic web search, URL fetching and YouTube video metadata),
+;; - local files (read/write/edit),
+;; - the state of Emacs (documentation and Elisp evaluation),
+;; - and Bash, if you are in a POSIX-y environment.
+;;
+;; To use gptel-agent in a dedicated buffer:
 ;;
 ;; - Set `gptel-model' and `gptel-backend' to use your preferred LLM.
 ;;
@@ -42,8 +48,10 @@
 ;;   can reset the agent state by (re)applying the "gptel-agent" preset from
 ;;   gptel's menu.
 ;;
+;; To use gptel-agent anywhere in Emacs:
+;;
 ;; - As with gptel, you can use gptel-agent in any buffer.  Just apply the
-;;   "gptel-agent" preset in the buffer.
+;;   "gptel-agent" preset in the buffer, or include "@gptel-agent" in a prompt.
 ;;
 ;; gptel-agent can delegate tasks to "sub-agents".  Sub-agents can be specified
 ;; in Markdown or Org files in a directory.  To see how to specify agents,
